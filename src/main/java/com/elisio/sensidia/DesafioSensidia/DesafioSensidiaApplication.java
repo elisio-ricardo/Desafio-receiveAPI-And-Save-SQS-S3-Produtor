d0c763491198d6c5163ac068dde0a1e0446ae2e5
Elisio Ricardo <elisio.silva@sysmap.com>
2024-09-11 17:30:52 -0300

    adicionando codigo initializer

---
+++ src/main/java/com/elisio/sensidia/DesafioSensidia/DesafioSensidiaApplication.java
@@ -0,0 +1,13 @@
+package com.elisio.sensidia.DesafioSensidia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSensidiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSensidiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elisio/sensidia/DesafioSensidia/DesafioSensidiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elisio.sensidia.DesafioSensidia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSensidiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
